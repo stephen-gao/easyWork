415818ed42cfae5cf5cfba0a0f7fd9cda9ca2953
gaosheng <gaoshengok@sina.com>
2017-09-12 10:30:29 +0800

    delete setting

--- src/main/java/com/gs/teacher/service/ITeacherService.java
+++ src/main/java/com/gs/teacher/service/ITeacherService.java
@@ -1,9 +1,12 @@
 package com.gs.teacher.service;
 
 import com.github.pagehelper.PageInfo;
+import com.gs.teacher.povo.Teacher;
 import com.gs.teacher.povo.TeacherVO;
 
 public interface ITeacherService {
 
 	PageInfo<TeacherVO> findTeacherPage(TeacherVO vo);
+	
+	void save(Teacher po);
 }
--- src/main/java/com/gs/teacher/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/gs/teacher/service/impl/TeacherServiceImpl.java
@@ -9,6 +9,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.gs.framework.constant.Constant;
 import com.gs.teacher.dao.ITeacherDao;
+import com.gs.teacher.povo.Teacher;
 import com.gs.teacher.povo.TeacherVO;
 import com.gs.teacher.service.ITeacherService;
 
@@ -23,6 +24,11 @@ public class TeacherServiceImpl implements ITeacherService{
 		PageHelper.startPage(null!=vo.getPageNum()?vo.getPageNum():Constant.PAGE_NUMBER.getNumber(), null!=vo.getPageSize()?vo.getPageSize():Constant.PAGE_SIZE.getNumber());
 		List<TeacherVO> list = teacherDao.findAll();
 		return new PageInfo<TeacherVO>(list);
+	}
+
+	@Override
+	public void save(Teacher po) {
+		teacherDao.insert(po);
 	}
 	
 	
